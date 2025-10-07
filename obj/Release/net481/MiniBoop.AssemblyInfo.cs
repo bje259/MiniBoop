2301449b998cc17daf1033e57872c8bb4d476899
Bradley Euell <bradley.euell@gmail.com>
2025-10-07 12:34:49 -0500

    rebuild release

--- obj/Release/net481/MiniBoop.AssemblyInfo.cs
+++ obj/Release/net481/MiniBoop.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniBoop")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b004d85a123a40d3846bbb657e9913eae94f4d50")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniBoop")]
